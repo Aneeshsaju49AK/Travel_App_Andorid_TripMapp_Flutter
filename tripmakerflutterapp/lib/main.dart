9877e82425689eec838aeb3b33344ed5283a5f62
AneeshSaju <aneeshsaju73@gmail.com>
2024-03-28 02:00:44 +0530

    added icons and changed the app name

--- tripmakerflutterapp/lib/main.dart
+++ tripmakerflutterapp/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:flutter/material.dart';
-
 import 'package:hive_flutter/hive_flutter.dart';
 import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
